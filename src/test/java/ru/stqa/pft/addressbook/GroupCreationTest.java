07f1eee374ca2c86a82e7aeb1b926ce9525b6df0
Svyatoslav Vozgrin <Svjatoslav.Vozgrin@gmail.com>
2018-11-30 15:34:21 +0200

    Lesson 23

--- src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
+++ src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
@@ -3,60 +3,11 @@ package ru.stqa.pft.addressbook;
 import org.testng.annotations.Test;
 
 
-<<<<<<< HEAD
-import static org.testng.Assert.fail;
-
-
-public class GroupCreationTest {
-    private WebDriver driver;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
-
-    @BeforeClass(alwaysRun = true)
-    public void setUp() throws Exception {
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.get("http://localhost/addressbook/group.php");
-        driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys("admin");
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys("secret");
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
-    }
-=======
 public class GroupCreationTest extends TestBase {
->>>>>>> Lesson 23
 
     @Test
     public void testGroupCreation() throws Exception {
 
-<<<<<<< HEAD
-        driver.findElement(By.linkText("groups")).click();
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).clear();
-        driver.findElement(By.name("group_name")).sendKeys("Name");
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).clear();
-        driver.findElement(By.name("group_header")).sendKeys("Header");
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).clear();
-        driver.findElement(By.name("group_footer")).sendKeys("Footer");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("group page")).click();
-    }
-
-    @AfterClass(alwaysRun = true)
-    public void tearDown() throws Exception {
-        driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
-    }
-=======
         gotoGroupPage();
         initGroupCreation();
         fillGroupForm(new GroupData("Test1", "Test2", "Test3"));
@@ -64,6 +15,5 @@ public class GroupCreationTest extends TestBase {
         returnToGroupPage();
     }
 
->>>>>>> Lesson 23
 
 }
