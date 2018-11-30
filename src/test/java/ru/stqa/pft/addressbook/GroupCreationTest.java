42eb7fa7c0bf2db9cd5e09913cb8f8d9ddb572c5
Svyatoslav Vozgrin <Svjatoslav.Vozgrin@gmail.com>
2018-11-30 11:54:18 +0200

    Lesson 22_1

--- src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
+++ src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
@@ -2,7 +2,6 @@ package ru.stqa.pft.addressbook;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
